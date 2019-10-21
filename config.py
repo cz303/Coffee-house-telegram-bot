103484226c5d212bc5d94593f729198e4be5f14e
Бузанов Азамат Аскарович <aabuzanov@kst.cvetnaya.kz>
2019-10-21 09:59:16 +0600

    changes for config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ S_SPECIAL_DRINKS = 'Особые напитки'
 S_COFFEE = 'Кофе'
 S_HOT_DRINKS = 'Горячие напитки'
 
-# КОНСТАНТЫ ТОВАРОВ
+
 S_ITEM_SELECTION = 'Выбор товара'
 
 
@@ -30,10 +30,7 @@ S_ADD_ITEM = 'Товар добавлен в корзину'
 S_PAYMENT = 'Меню оплаты'
 
 S_MAKE_ORDER = 'Сделать заказ'
-
-
 S_SPECIAL_DRINKS_CHOICE = 'Выбор особых напитков'
-
 
 
 S_LATTE_LAVANDA_SHALFEI = 'Латте Лаванда Шалфей'
@@ -55,58 +52,20 @@ S_TEA = 'Чай'
 S_TEA_CHOICE = 'Чай Выбор'
 
 
-
-S_LATTE_LAVANDA_SHALFEI_300 = '300 мл - 139 руб'
-S_LATTE_LAVANDA_SHALFEI_400 = '400 мл - 169 руб'
-
-S_RAF_LEMON_PIE_300 = 'Раф Лимонный Пай 300 мл - 159 руб'
-S_RAF_LEMON_PIE_400 = 'Раф Лимонный Пай 400 мл - 179 руб'
-
-S_KAPUCHINO_300 = 'Капучино 300 мл - 119 руб'
-S_KAPUCHINO_400 = 'Капучино 400 мл - 149 руб'
-
-S_LATTE_MAKIATO_300 = 'Латте Макиато 300 мл - 119 руб'
-S_LATTE_MAKIATO_400 = 'Латте Макиато 400 мл - 139 руб'
-
-S_KAKAO_300 = 'Какао 300 мл - 119 руб'
-S_KAKAO_400 = 'Какао 400 мл - 149 руб'
-
-S_TEA_ENGLISH_300 = 'Чай 300 мл - 99 руб'
-S_TEA_ENGLISH_400 = 'Чай 400 мл - 109 руб'
-
-S_TEA_ERL_300 = 'Чай Эрл Грей 300 мл - 99 руб'
-S_TEA_ERL_400 = 'Чай Эрл Грей 400 мл - 109 руб'
-
-#print(list(gh_menu[S_HOT_DRINKS][S_TEA][S_ENGLISH_BREAKFAST]))
-#print(gh_menu[S_HOT_DRINKS][S_TEA].get(99))
-
-
-#ОСОБЫЕ НАПИТКИ
-list_of_latte = ['Латте Лаванда Шалфей']
-
-'''print(gh_menu[S_SPECIAL_DRINKS][S_LATTE_LAVANDA_SHALFEI]['300 мл - 139 руб'])
-for k,v in gh_menu.items(): 
-    print(list(v.keys()))'''
-
 def create_menu(mass, back = True):
-    """
-    This function allows to creat menu of buttons.
-    mass - the list of string (buttons)
-    back - back button, if true, add a button back. Default back=True
-    """
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     if len(mass) == 1:
-        markup.row(mass[0]) #Если массив содержит только 1 элемент, то просто добавить его
+        markup.row(mass[0]) 
     else:
         while len(mass) > 0:  
             try:
-                cut = mass[:2] #Берем только первые два элемента массива и добавляем их в клавиатуру
+                cut = mass[:2] 
                 markup.row(cut[0], cut[1])
-                del mass[:2] #Удаляем эти элементы, так как их уже добавили
+                del mass[:2] 
                 
                 if len(mass) == 1:
                     markup.row(mass[0])
-                    break #Если массив содержит 1 элемент, то добавить его и выйти из цикла
+                    break
             except:
                 print('WTF')
     
@@ -122,11 +81,6 @@ class Goods:
         self.bot = bot
         self.message = message 
         self.state = state
-
-        #self - ссылка на экземпляр этого класса 
-        #bot - это объект нашего бота, который мы создаем в самом начале файла bot.py (bot = telebot.TeleBot(config.TOKEN) )
-        #message - это объект библиотеки pyTelegramBotAPI, он нужен для получения данных о пользователе, отправленным им текстом и еще много всего.
-        #state - состояние пользователя, которое мы хотим ему присвоить
 
     def get_goods_list(self):
         user_id = self.message.from_user.id
